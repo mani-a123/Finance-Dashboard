fb7f6639e158eaa9e3aaa8a7f4826ccb4763aff7
mani-a123 <mlgsmanikantaaddala@gmail.com>
2025-09-01 14:34:15 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import API from "../api";
-import { Link } from 'react-router-dom';
+
 
 export default function Login({ setToken }) {
   const [email, setEmail] = useState("");
